326be529f8bd95680f5b6d4a20e347c4cf93b119
lomac20 <152649910+LoreneMadelaineCaprice@users.noreply.github.com>
2024-08-05 22:31:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+from datetime import time, datetime
+from datetime import timedelta
+from time import sleep
+import winsound
+print('Entrez votre miniteur')
 
+heure = int(input('H:'))
+minute = int(input('MIN:'))
+seconde = int(input('S:'))
+
+temps = time(heure, minute, seconde)
+
+maintenant = datetime.now()
+duree = timedelta(hours= heure, minutes= minute, seconds= seconde)
+futur = maintenant + duree
+
+
+print('Début du minuteur',maintenant)
+
+sleep(duree.total_seconds())
+winsound.PlaySound("SystemHand", winsound.SND_LOOP)
+print(futur)
